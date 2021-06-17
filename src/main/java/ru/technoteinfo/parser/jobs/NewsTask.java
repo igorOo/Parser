33ab8926e0f94792325a9ecf4463abd3d97b2435
igor <mail@mail.ru>
2021-06-17 23:54:28 +0300

    Сохранение изображений в тексте через стримы и замена ссылки на файлы в тексте

--- src/main/java/ru/technoteinfo/parser/jobs/NewsTask.java
+++ src/main/java/ru/technoteinfo/parser/jobs/NewsTask.java
@@ -11,14 +11,23 @@ import org.springframework.stereotype.Component;
 import ru.technoteinfo.parser.Entity.News;
 import ru.technoteinfo.parser.Services.NewsService;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import javax.imageio.ImageIO;
+import javax.imageio.stream.ImageInputStream;
+import javax.imageio.stream.ImageOutputStream;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Random;
 
 @Component
 public class NewsTask {
@@ -28,6 +37,7 @@ public class NewsTask {
 
     @Scheduled(fixedDelay = 1000*60*20)
     public void parse(){
+
         System.out.println("Начало парсинга");
 
         String domain = "https://pcnews.ru";
@@ -42,42 +52,86 @@ public class NewsTask {
                 html.append(line);
             }
 
-
             doc = Jsoup.parse(html.toString());
 
             Elements elements = doc.select(".title");
             for (Element element: elements ) {
                 Elements links = element.getElementsByTag("a");
                 for (Element link: links){
-                    Document subDoc = Jsoup.connect(domain+"/"+link.attr("href"))
-                            .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0")
-                            .timeout(10000)
-                            .get();
-                    Element box = subDoc.getElementById("article-box");
-                    String dateCreate = box.select("h1").select(".meta-data").text();
-                    box.select("h1").select(".meta-data").remove();
-                    final String title = box.select("h1").eachText().get(0);
-                    box.select("h1").remove();
-                    box.lastElementSibling().remove();
-                    List<Node> nodes = box.childNodes();
-                    StringBuilder text = new StringBuilder();
-                    for (Node node: nodes) {
-                        text.append(node.toString().trim());
-                    }
+                        Document subDoc = null;
+                        try {
+                            subDoc = Jsoup.connect(domain+"/"+link.attr("href"))
+                                    .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0")
+                                    .timeout(50000)
+                                    .get();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        if (subDoc == null) {
+                            return;
+                        }
+                        Element box = subDoc.getElementById("article-box");
+                        String dateCreate = box.select("h1").select(".meta-data").text();
+                        box.select("h1").select(".meta-data").remove();
+                        final String title = box.select("h1").eachText().get(0);
+                        box.select("h1").remove();
+                        box.lastElementSibling().remove();
+                        List<Element> nodes = box.children();
+                        StringBuilder text = new StringBuilder();
+                        for (Element node: nodes) {
+                            Elements imgs = node.getElementsByTag("img");
+                            if (!imgs.isEmpty()){
+                                for (Element img: imgs){
+                                    String urlImg = img.attr("src");
+                                    String[] parsedUrl = urlImg.split("\\.");
+                                    URL website = null;
+                                    try {
+                                        website = new URL(urlImg);
+                                        try (InputStream stream = website.openStream()) {
+
+                                            File outputfile = new File("d:/pic/"+generateRandomString(12)+".jpg");
+                                            OutputStream outStream = new FileOutputStream(outputfile);
+                                            outStream.write(stream.readAllBytes());
+                                            img.attr("src", outputfile.toString());
+                                        }catch (IOException error){
+                                            error.printStackTrace();
+                                        }
+                                    } catch (MalformedURLException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }
+                            text.append(node.toString().trim());
+                        }
 //                    final String text = ((TextNode) box.childNodes()).text();
-                    if (!newsService.isExist(title)){
-                        News news = new News(
-                                title,
-                                text.toString(),
-                                LocalDateTime.parse(dateCreate, DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"))
-                        );
-                        newsService.save(news);
-                    }
+                        if (!newsService.isExist(title)){
+                            News news = new News(
+                                    title,
+                                    text.toString(),
+                                    LocalDateTime.parse(dateCreate, DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"))
+                            );
+                            newsService.save(news);
+                        }
                 }
             }
             System.out.println("Конец парсинга");
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    protected String generateRandomString(int length) {
+        if(length == 0){
+            length = 12;
+        }
+        String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+        while (salt.length() < length) { // length of the random string.
+            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+            salt.append(SALTCHARS.charAt(index));
+        }
+        return salt.toString();
+
     }
 }
